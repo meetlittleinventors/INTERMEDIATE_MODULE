d1ddc39c2ead94b4f2006dc6ca78fc9eaa213205
meetlittleinventors <meet@littleinventors.live>
2025-09-01 17:57:05 +0530

    Update and rename BankDemo.java to Student.java

---
+++ ClassAttributes/TeacherActivity/Demo2/Student.java
@@ -0,0 +1,18 @@
+class Student {
+    String name;
+    int marks;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Amit";
+        s1.marks = 85;
+
+        System.out.println("Before update: " + s1.name + " scored " + s1.marks);
+
+        // updating marks
+        s1.marks = 90;
+        System.out.println("After update: " + s1.name + " scored " + s1.marks);
+    }
+}
