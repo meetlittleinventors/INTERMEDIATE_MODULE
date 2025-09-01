ce1f822c50d664497c518ca26a8a3ea781c65027
meetlittleinventors <meet@littleinventors.live>
2025-09-01 18:15:44 +0530

    Update and rename Main1.java to Student.java

---
+++ Constructors/TeacherActivity/Demo1/Student.java
@@ -0,0 +1,12 @@
+class Student {
+    Student() {
+        System.out.println("Constructor called: Object Created!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+    }
+}
