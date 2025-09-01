b7e0943edc1ae516b4650848e36aed1156c09da9
meetlittleinventors <meet@littleinventors.live>
2025-09-01 18:24:36 +0530

    Update and rename MiniProject.java to Student.java

---
+++ Constructors/StudentActivity/Hard/Student.java
@@ -0,0 +1,29 @@
+class Student {
+    String name;
+    int marks;
+
+    Student(String n, int m) {
+        name = n;
+        marks = m;
+    }
+
+    // Copy Constructor
+    Student(Student s) {
+        name = s.name;
+        marks = s.marks;
+    }
+
+    void show() {
+        System.out.println(name + " scored " + marks);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Amit", 85);
+        Student s2 = new Student(s1); // using copy constructor
+
+        s1.show();
+        s2.show();
+    }
+}
