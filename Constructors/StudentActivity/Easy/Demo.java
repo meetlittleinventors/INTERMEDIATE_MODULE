84e8416175e7b8e5fd6d30a1ca19e612dd32ff06
meetlittleinventors <meet@littleinventors.live>
2025-09-01 18:21:52 +0530

    Update and rename Easy1.java to Demo.java

---
+++ Constructors/StudentActivity/Easy/Demo.java
@@ -0,0 +1,11 @@
+class Demo {
+    Demo() {
+        System.out.println("Welcome to Java Constructors!");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Demo d1 = new Demo();
+    }
+}
