5be07dc21d193c163984c6737e53101762745230
meetlittleinventors <meet@littleinventors.live>
2025-09-01 17:56:46 +0530

    Update and rename StudentDemo.java to Student.java

--- ClassAttributes/TeacherActivity/Demo1/Student.java
+++ ClassAttributes/TeacherActivity/Demo1/Student.java
@@ -3,21 +3,18 @@ class Student {
     int marks;
 }
 
-public class StudentDemo {
+public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
+        Student s2 = new Student();
+
         s1.name = "Amit";
         s1.marks = 85;
 
-        Student s2 = new Student();
-        s2.name = "Neha";
+        s2.name = "Riya";
         s2.marks = 92;
 
         System.out.println(s1.name + " scored " + s1.marks);
         System.out.println(s2.name + " scored " + s2.marks);
-
-        // Update marks
-        s1.marks = 90;
-        System.out.println(s1.name + " new marks: " + s1.marks);
     }
 }
