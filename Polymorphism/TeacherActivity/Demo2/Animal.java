7afd3344aee9331f841c5d8b3787503238f151c3
meetlittleinventors <meet@littleinventors.live>
2025-09-02 21:34:44 +0530

    Update and rename Main.java to Animal.java

---
+++ Polymorphism/TeacherActivity/Demo2/Animal.java
@@ -0,0 +1,26 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void makeSound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        a1.makeSound();
+        a2.makeSound();
+    }
+}
