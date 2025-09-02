cf3be116ccf88bf4ee3f884372645099aee1c64a
meetlittleinventors <meet@littleinventors.live>
2025-09-02 21:35:44 +0530

    Update Test.java

--- Polymorphism/TeacherActivity/Demo2/Test.java
+++ Polymorphism/TeacherActivity/Demo2/Test.java
@@ -1,26 +1,26 @@
-abstract class Shape {
-    abstract void area();
-}
-
-class Circle extends Shape {
-    double radius = 5;
-    void area() {
-        System.out.println("Circle Area = " + (3.14 * radius * radius));
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
     }
 }
 
-class Square extends Shape {
-    int side = 4;
-    void area() {
-        System.out.println("Square Area = " + (side * side));
+class Dog extends Animal {
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    void makeSound() {
+        System.out.println("Cat meows");
     }
 }
 
 public class Test {
     public static void main(String[] args) {
-        Shape s1 = new Circle();
-        Shape s2 = new Square();
-        s1.area();
-        s2.area();
+        Animal a1 = new Dog();
+        Animal a2 = new Cat();
+        a1.makeSound();
+        a2.makeSound();
     }
 }
