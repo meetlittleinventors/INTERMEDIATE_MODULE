f01acb299e69b13cb2661b228c21b0ef72d7ca36
meetlittleinventors <meet@littleinventors.live>
2025-10-01 21:29:25 +0530

    Update Animal.java

--- Inheritance/TeacherActivity/Demo5/Animal.java
+++ Inheritance/TeacherActivity/Demo5/Animal.java
@@ -1,17 +1,24 @@
+// Hybrid Inheritance Example
 class Animal {
-    Animal() {
-        System.out.println("Animal created");
-    }
-}
-class Dog extends Animal {
-    Dog() {
-        super(); // calling parent constructor
-        System.out.println("Dog created");
+    void eat() {
+        System.out.println("Animal eats food.");
     }
 }
 
-public class Test {
+interface Pet {
+    void beFriendly();
+}
+
+class Dog extends Animal implements Pet {
+    public void beFriendly() {
+        System.out.println("Dog is friendly.");
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
         Dog d = new Dog();
+        d.eat();
+        d.beFriendly();
     }
 }
